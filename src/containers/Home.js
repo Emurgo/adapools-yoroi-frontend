d5c7667d668172736a9a1babeae1a5e22d7bc156
Paul Ccari <paulclindo@gmail.com>
2020-08-18 23:27:34 -0500

    fixed destructuring selected ids array

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -138,7 +138,7 @@ function Home(props: HomeProps): Node {
     });
   };
   
-  const { selectedPoolIds } = props
+  const { props: { urlParams: { selectedPoolIds } } } = props
   return (
     <Layout>
       <Header>
