9789acac4b442e91f5051f765514b35d8ff782f7
Paul Ccari <paulclindo@gmail.com>
2020-11-03 18:04:04 -0500

    formatting code, and added noopener on anchor tags for vulnerabilities

--- src/components/StakingPoolCard.js
+++ src/components/StakingPoolCard.js
@@ -57,23 +57,17 @@ type Props = {|
   tickerName: string,
   name: string,
   links: {|
-    tw:?string, 
-    tg:?string, 
-    fb:?string, 
-    yt:?string, 
-    tc:?string, 
-    di:?string, 
-    gh:?string, 
-  |}
+    tw: ?string,
+    tg: ?string,
+    fb: ?string,
+    yt: ?string,
+    tc: ?string,
+    di: ?string,
+    gh: ?string,
+  |},
 |};
 
-function StakingPoolCard({
-  id,
-  avatar,
-  tickerName,
-  name,
-  links,
-  fullname }: Props): Node {
+function StakingPoolCard({ id, avatar, tickerName, name, links, fullname }: Props): Node {
   function truncateString(string: string): string {
     if (string.length <= 8) {
       return string;
@@ -81,70 +75,74 @@ function StakingPoolCard({
     return `${string.substring(0, 6)}...${string.slice(string.length - 2)}`;
   }
 
-  const twitter = links && links.tw
-  const telegram = links && links.tg
-  const facebook = links && links.fb
-  const youtube = links && links.yt
-  const twitch = links && links.tc
-  const discord = links && links.di
-  const github = links && links.gh
-  const websiteUrl = fullname.match(/(https?:\/\/[^\s]+"?utm_source=adapools.org)/g)
-  
+  const twitter = links && links.tw;
+  const telegram = links && links.tg;
+  const facebook = links && links.fb;
+  const youtube = links && links.yt;
+  const twitch = links && links.tc;
+  const discord = links && links.di;
+  const github = links && links.gh;
+  const websiteUrl = fullname.match(/(https?:\/\/[^\s]+"?utm_source=adapools.org)/g);
+
   return (
     <MainCardPool>
       <div className="card-image">
-        { avatar ?
-          <img src={avatar} alt="" />
-          :
-          <SVG src={toSvg(id, 42)} />
-        }
+        {avatar ? <img src={avatar} alt="" /> : <SVG src={toSvg(id, 42)} />}
       </div>
       <div className="card-info">
         <a
           href={`https://adapools.org/pool/${id}`}
           className="name"
           target="_blank"
-          rel="noreferrer"
+          rel="noreferrer noopener"
         >
-          {tickerName ?  <>[{tickerName}]{' '}</> : null}
+          {tickerName ? <>[{tickerName}] </> : null}
           {name || ''}
         </a>
         <DesktopOnly>
           <span className="id"> {truncateString(id)}</span>
         </DesktopOnly>
         <span className="social-media">
-          {twitter ? 
-            <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noreferrer">
+          {twitter ? (
+            <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-twitter" />
-            </a> :null}
-          {telegram ? 
-            <a href={`https://t.me/${telegram}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {telegram ? (
+            <a href={`https://t.me/${telegram}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-telegram" />
-            </a> :null}
-          {facebook ? 
-            <a href={`https://fb.me/${facebook}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {facebook ? (
+            <a href={`https://fb.me/${facebook}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-facebook" />
-            </a> :null}
-          {youtube ? 
-            <a href={`https://youtube.com/${youtube}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {youtube ? (
+            <a href={`https://youtube.com/${youtube}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-youtube" />
-            </a> :null}
-          {twitch ? 
-            <a href={`https://twitch.com/${twitch}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {twitch ? (
+            <a href={`https://twitch.com/${twitch}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-twitch" />
-            </a> :null}
-          {discord ? 
-            <a href={`https://discord.gg/${discord}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {discord ? (
+            <a href={`https://discord.gg/${discord}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-server" />
-            </a> :null}
-          {github ? 
-            <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {github ? (
+            <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-github-alt" />
-            </a> :null}
-          {websiteUrl ? 
-            <a href={websiteUrl} target="_blank" rel="noreferrer">
+            </a>
+          ) : null}
+          {websiteUrl ? (
+            <a href={websiteUrl} target="_blank" rel="noreferrer noopener">
               <i className="fa fa-link" />
-            </a> :null}
+            </a>
+          ) : null}
         </span>
       </div>
     </MainCardPool>
