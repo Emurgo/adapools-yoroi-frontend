ec796d68953b90240a7ecc74f47ae4b108a33e3d
Paul Ccari <paulclindo@gmail.com>
2020-11-03 18:05:51 -0500

    remove unused font imports in global styles

--- src/helpers/globalStyles.js
+++ src/helpers/globalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
   html,
 body,
 h1,
