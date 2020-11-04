0fc39ff777cc7bee8d7329f927571e251c1cbd2d
Paul Ccari <46382556+paulclindo@users.noreply.github.com>
2020-11-04 02:03:44 -0500

    Paulccari/removing html parser (#54)

--- src/components/StakingPoolCard.js
+++ src/components/StakingPoolCard.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import type { Node } from 'react';
 import styled from 'styled-components';
-import parse from 'html-react-parser';
 import { toSvg } from 'jdenticon';
 import SVG from 'react-inlinesvg';
 import { DesktopOnly } from './layout/Breakpoints';
@@ -12,81 +11,139 @@ const MainCardPool = styled.div`
   padding: 15px 0;
   .card-image {
     width: 45px;
+    min-width: 45px;
     margin-right: 15px;
     img {
       width: 100%;
     }
   }
   .card-info {
+    display: flex;
+    flex-wrap: wrap;
     color: #1a44b7;
     font-size: 16px;
     letter-spacing: 0;
     line-height: 22px;
-    span {
-      margin-left: 8px;
-      color: #676970;
-      font-size: 14px;
-      line-height: 22px;
+    a {
+      display: block;
+    }
+    i {
+      font-size: 16px;
     }
   }
   .name {
-    display: flex;
-    align-items: flex-start;
+    margin-right: 8px;
+    margin-bottom: 4px;
   }
   .social-media {
-    display: grid;
-    grid-template-columns: repeat(6, auto);
+    display: flex;
+    flex: 1 1 60%;
     a {
       margin-right: 10px;
-    }
-    a[href*="https://adapools.org"] {
-      grid-column: -1/1;
-      grid-row: 1;
-      margin-bottom: 6px;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      overflow: hidden; 
-    }
-    a[class*="fa"] {
-
+      font-size: 20px;
     }
   }
   .id {
+    color: #676970;
+    line-height: 22px;
+    font-size: 14px;
+    margin-right: 13px;
   }
 `;
 type Props = {|
   avatar: string,
   fullname: string,
   id: string,
+  tickerName: string,
+  name: string,
+  links: {|
+    tw: ?string,
+    tg: ?string,
+    fb: ?string,
+    yt: ?string,
+    tc: ?string,
+    di: ?string,
+    gh: ?string,
+  |},
 |};
 
-function StakingPoolCard({ avatar, fullname, id }: Props): Node {
-  function truncateString(string) {
+function StakingPoolCard({ id, avatar, tickerName, name, links, fullname }: Props): Node {
+  function truncateString(string: string): string {
     if (string.length <= 8) {
       return string;
     }
     return `${string.substring(0, 6)}...${string.slice(string.length - 2)}`;
   }
 
+  const twitter = links && links.tw;
+  const telegram = links && links.tg;
+  const facebook = links && links.fb;
+  const youtube = links && links.yt;
+  const twitch = links && links.tc;
+  const discord = links && links.di;
+  const github = links && links.gh;
+  const websiteUrl = fullname.match(/(https?:\/\/[^\s]+"?utm_source=adapools.org)/g);
+
   return (
     <MainCardPool>
       <div className="card-image">
-        { avatar ?
-          <img src={avatar} alt="" />
-          :
-          <SVG src={toSvg(id, 42)} />
-        }
+        {avatar ? <img src={avatar} alt="" /> : <SVG src={toSvg(id, 42)} />}
       </div>
       <div className="card-info">
-        <div className="name">
-          <div className="social-media">
-            {parse(fullname)}
-            <DesktopOnly>
-              <span className="id"> {truncateString(id)}</span>
-            </DesktopOnly>
-          </div>
-        
-        </div>
+        <a
+          href={`https://adapools.org/pool/${id}`}
+          className="name"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {tickerName ? <>[{tickerName}] </> : null}
+          {name || ''}
+        </a>
+        <DesktopOnly>
+          <span className="id"> {truncateString(id)}</span>
+        </DesktopOnly>
+        <span className="social-media">
+          {twitter ? (
+            <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-twitter" />
+            </a>
+          ) : null}
+          {telegram ? (
+            <a href={`https://t.me/${telegram}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-telegram" />
+            </a>
+          ) : null}
+          {facebook ? (
+            <a href={`https://fb.me/${facebook}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-facebook" />
+            </a>
+          ) : null}
+          {youtube ? (
+            <a href={`https://youtube.com/${youtube}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-youtube" />
+            </a>
+          ) : null}
+          {twitch ? (
+            <a href={`https://twitch.com/${twitch}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-twitch" />
+            </a>
+          ) : null}
+          {discord ? (
+            <a href={`https://discord.gg/${discord}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-server" />
+            </a>
+          ) : null}
+          {github ? (
+            <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-github-alt" />
+            </a>
+          ) : null}
+          {websiteUrl ? (
+            <a href={websiteUrl} target="_blank" rel="noreferrer noopener">
+              <i className="fa fa-link" />
+            </a>
+          ) : null}
+        </span>
       </div>
     </MainCardPool>
   );
