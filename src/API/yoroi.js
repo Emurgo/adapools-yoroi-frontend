a83046fd86b1aec882129b7c84ed53af632b80c1
Denis Nebytov <nebytov.d@gmail.com>
2025-06-17 20:40:50 +0300

    fix eslint notes

--- src/API/yoroi.js
+++ src/API/yoroi.js
@@ -1,56 +1,59 @@
 // @flow
 
-export type SelectedPools = Array<string>
+export type SelectedPools = Array<string>;
 
 export const Source = Object.freeze({
   CHROME_EXTENSION: 'chrome',
   FIREFOX_EXTENSION: 'firefox',
   MOBILE: 'mobile',
-})
-export type SourceType = $Values<typeof Source>
+});
+export type SourceType = $Values<typeof Source>;
 
 const relevantDataForYoroi = (selectedPools: SelectedPools) => {
-  return encodeURI(JSON.stringify(selectedPools))
-}
+  return encodeURI(JSON.stringify(selectedPools));
+};
 
 export type YoroiCallbackProps = {|
-    source: ?string,
-    chromeId: ?string,
-    mozId: ?string,
-|}
+  source: ?string,
+  chromeId: ?string,
+  mozId: ?string,
+|};
 
-export const YoroiCallback = (selectedPools: SelectedPools, yoroiCBProps: YoroiCallbackProps): void => {
+export const YoroiCallback = (
+  selectedPools: SelectedPools,
+  yoroiCBProps: YoroiCallbackProps,
+): void => {
   const { source } = yoroiCBProps;
 
-  let postMessage
-  const encodedDataForYoroi = relevantDataForYoroi(selectedPools)
+  let postMessage;
+  const encodedDataForYoroi = relevantDataForYoroi(selectedPools);
   switch (source) {
-  case Source.CHROME_EXTENSION:
-    window.parent.postMessage(
-      encodedDataForYoroi,
-      `chrome-extension://${yoroiCBProps.chromeId ?? ''}/main_window.html#/staking`
-    )
-    break
-  case Source.FIREFOX_EXTENSION:
-    window.parent.postMessage(
-      encodedDataForYoroi,
-      `moz-extension://${yoroiCBProps.mozId ?? ''}/main_window.html#/staking`
-    )
-    break
-  case Source.MOBILE:
-    postMessage = window.parent.postMessage
-    if (window.ReactNativeWebView) {
-      postMessage = function postToReactNative (data, _targetOrigin) {
-        window.ReactNativeWebView.postMessage(data)
+    case Source.CHROME_EXTENSION:
+      window.parent.postMessage(
+        encodedDataForYoroi,
+        `chrome-extension://${yoroiCBProps.chromeId ?? ''}/main_window.html#/staking`,
+      );
+      break;
+    case Source.FIREFOX_EXTENSION:
+      window.parent.postMessage(
+        encodedDataForYoroi,
+        `moz-extension://${yoroiCBProps.mozId ?? ''}/main_window.html#/staking`,
+      );
+      break;
+    case Source.MOBILE:
+      postMessage = window.parent.postMessage;
+      if (window.ReactNativeWebView) {
+        postMessage = function postToReactNative(data, _targetOrigin) {
+          window.ReactNativeWebView.postMessage(data);
+        };
       }
-    }
-    postMessage(encodedDataForYoroi, 'yoroi://simple-staking/selection')
-    break
-  default:
-    window.parent.postMessage(encodedDataForYoroi, source)
-    break
+      postMessage(encodedDataForYoroi, 'yoroi://simple-staking/selection');
+      break;
+    default:
+      window.parent.postMessage(encodedDataForYoroi, source);
+      break;
   }
-}
+};
 
 export const SendFirstAdapool = (firstPool: Object): void => {
   const poolInfo = {
@@ -58,8 +61,8 @@ export const SendFirstAdapool = (firstPool: Object): void => {
     name: `[${firstPool.db_ticker}] ${firstPool.db_name}`,
     roa: parseFloat(firstPool.roa) === 0 ? 'unknown' : `${parseFloat(firstPool.roa).toFixed(2)}% `,
     socialLinks: firstPool.handles,
-    avatar: firstPool.pool_pic
+    avatar: firstPool.pool_pic,
   };
   const encodedFirstPool = encodeURI(JSON.stringify(poolInfo));
   window.parent.postMessage(encodedFirstPool, '*');
-}
+};
